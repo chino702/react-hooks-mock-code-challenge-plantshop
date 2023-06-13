bab6c6e738110563251d59e0aaa3d1bcbc93a169
chino702 <chinox3702@gmail.com>
2023-06-13 16:24:49 -0700

    plants lab

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  useEffect(() => {
+    fetchPlants();
+  }, []);
+
+  const fetchPlants = async () => {
+    try {
+      const response = await fetch("http://localhost:6001/plants");
+      const data = await response.json();
+      setPlants(data);
+    } catch (error) {
+      console.log("Error fetching plants:", error);
+    }
+  };
+
   return (
     <main>
       <NewPlantForm />
-      <Search />
-      <PlantList />
+      <Search onChange={(query) => setSearchQuery(query)} />
+      <PlantList plants={plants} searchQuery={searchQuery} />
     </main>
   );
 }
